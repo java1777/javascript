6ab6edca1d982f1480a9c4b08b68ddfd358ad37b
Javohir <108736263+java1777@users.noreply.github.com>
2025-11-28 15:57:58 +0500

    feat: [if else operatciyalari]

--- index.js
+++ index.js
@@ -1,139 +1,294 @@
 // 1-Misol!
-let L = 717;
-let La = L / 100;
-console.log(parseInt(La));
+// let a = 17;
+
+// if (a > 0) {
+//     a = a + 1;
+// } else {
+
+// }
+
+// console.log(a);
 
 // 2-Misol!
-let M = 5751;
-let Ma = M / 1000;
-console.log(parseInt(Ma));
+// let a = +prompt ("manfiy yo musbat son kiriting!");
+
+// if (a > 0) {
+//     a = a + 1;
+// } else {
+//     a = a - 2;
+// }
+// console.log(a);
 
 // 3-Misol!
-let K = 71557;
-let Ka = K / 1024;
-console.log(parseInt(Ka));
+// let a = +prompt("Son kiriting:");
+
+// if (a > 0) a++;
+// else if (a < 0) a -= 2;
+// else a = 10;
+
+// console.log(a);
 
 // 4-Misol!
-let q = 77;
-let w = 7;
-let e = q / w;
-console.log(parseInt(e));
+// let a = 11;
+// let b = -10;
+// let c = 17;
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+// if (b > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+// if (c > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+// console.log(musbat);
 
 // 5-Misol!
-let r = 757;
-let t = 7;
-console.log(parseInt(r / t) % 10);
+// let a = 11;
+// let b = -10;
+// let c = 17;
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+// if (b > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+// if (c > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+// console.log(musbat , manfiy)
 
 // 6-Misol!
-let y = 75;
-let u = y % 10;
-let i = y / 10;
+// let a = +prompt("1-sonni kiriting:");
+// let b = +prompt("2-sonni kiriting:");
 
-console.log(parseInt(i), "and",  parseInt(u));
+// if (a > b) {
+//   console.log("Katta son:", a);
+// } else if (b > a) {
+//   console.log("Katta son:", b);
+// } else {
+//   console.log("Ikkala son teng");
+// }
 
 // 7-Misol!
-let o = 89;
-let p = o / 10;
-let s = o % 10;
-let d = p + s ;
-console.log(parseInt(d));
+// let a = +prompt("1-sonni kiriting:");
+// let b = +prompt("2-sonni kiriting:");
+
+// if (a < b) {
+//   console.log("Kichik sonning tartib raqami: 1");
+// } else if (b < a) {
+//   console.log("Kichik sonning tartib raqami: 2");
+// } else {
+//   console.log("Ikkala son teng, tartib raqami yo");
+// }
 
 // 8-Misol!
-let f = 64;
-let g = f / 10;
-let h = f % 10 ;
-let j = h * 10 + g;
-console.log(parseInt(j));
+// let a = +prompt("a:");
+// let b = +prompt("b:");
+
+// if (a > b) {
+//   console.log(a, b);
+// } else if (b > a) {
+//   console.log(b, a);
+// } else {
+//   console.log("Teng");
+// }
 
 // 9-Misol!
-let k = 457; 
-let l = k / 100;
-console.log(parseInt(l));
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+
+// if (A > B) {
+//   let temp = A;
+//   A = B;
+//   B = temp;
+// }
+
+// console.log("A =", A, "B =", B);
 
 // 10-Misol!
-let z = 575;
-let x = z % 100 % 10;
-let v = z / 10 % 10;
-console.log((x));
-console.log(parseInt(v));
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+
+// if (A !== B) {
+//   let yigindi = A + B;
+//   A = yigindi;
+//   B = yigindi;
+// } else {
+//   A = 0;
+//   B = 0;
+// }
+
+// console.log("A =", A, "B =", B);
 
 // 11-Misol!
-let n = 147;
-let m = n / 100 ;
-let qq = n % 100 / 10;
-let ww = n % 10;
-let ee = m + qq + ww;
-console.log(parseInt(m) + parseInt(qq) + (ww));
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+
+// if (A !== B) {
+//   let max = A > B ? A : B;
+//   A = max;
+//   B = max;
+// } else {
+//   A = 0;
+//   B = 0;
+// }
+
+// console.log("A =", A, "B =", B);
 
 // 12-Misol!
-let rr = 157;
-console.log(rr % 10);
-console.log(parseInt (rr % 100 / 10));
-console.log(parseInt(rr / 100));
+// let a = +prompt("a:");
+// let b = +prompt("b:");
+// let c = +prompt("c:");
+
+// let min = a;
+
+// if (b < min) min = b;
+// if (c < min) min = c;
+
+// console.log("Kichik son:", min);
 
 // 13-Misol!
-let tt = 385;
-let yy = tt / 100;
-let uu = tt % 100 * 10 + yy;
-console.log(parseInt(uu));
+// let a = +prompt("a:");
+// let b = +prompt("b:");
+// let c = +prompt("c:");
+
+// let orta;
+
+// if ((a > b && a < c) || (a > c && a < b)) orta = a;
+// else if ((b > a && b < c) || (b > c && b < a)) orta = b;
+// else orta = c;
+
+// console.log("Ortacha son:", orta);
 
 // 14-Misol!
-let ii = 717;
-let oo = ii / 100;
-let pp = ii % 100;
-let aa = pp * 10 + oo;
-console.log(parseInt(aa));
+// let a = +prompt("a:");
+// let b = +prompt("b:");
+// let c = +prompt("c:");
+
+// let min = Math.min(a, b, c);
+// let max = Math.max(a, b, c);
+
+// console.log("Kichigi:", min);
+// console.log("Kattasi:", max);
 
 // 15-Misol!
-let ss = 123;
-let dd = ss / 100 ;
-let ff = (ss % 100 / 10) ;
-let gg = ss % 10;
-console.log(parseInt(ff) * 100 + parseInt(dd) * 10 + (gg));
+// let a = +prompt("a:");
+// let b = +prompt("b:");
+// let c = +prompt("c:");
+
+// let AB = a + b;
+// let AC = a + c;
+// let BC = b + c;
+
+// if (AB >= AC && AB >= BC) {
+//   console.log("Eng katta yigindi:", a, b);
+// } else if (AC >= AB && AC >= BC) {
+//   console.log("Eng katta yigindi:", a, c);
+// } else {
+//   console.log("Eng katta yigindi:", b, c);
+// }
 
 // 16-Misol!
-let hh = 123;
-let jj = hh / 100;
-let kk = (hh % 100 % 10) * 10;
-let ll = hh % 100 / 10
-console.log(parseInt(jj) * 100 + (kk) + parseInt(ll));
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+// let C = +prompt("C:");
+
+// if (A < B && B < C) {
+//   A *= 2;
+//   B *= 2;
+//   C *= 2;
+// } else {
+//   A = -A;
+//   B = -B;
+//   C = -C;
+// }
+
+// console.log("Natija:", A, B, C);
 
 // 17-Misol!
-let a = 1775;
-console.log(parseInt(a / 100) % 10);
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+// let C = +prompt("C:");
+
+// if (A > B && B > C) {
+//   A *= 2;
+//   B *= 2;
+//   C *= 2;
+// } else {
+//   A = -A;
+//   B = -B;
+//   C = -C;
+// }
+
+// console.log("Natija:", A, B, C);
 
 // 18-Misol!
-let zz = 1457;
-let xx = zz / 1000;
-let cc = zz % 1000;
-console.log(parseInt(xx));
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+// let C = +prompt("C:");
+
+// if (A === B && B !== C) {
+//   console.log(3);
+// } else if (A === C && C !== B) {
+//   console.log(2);
+// } else if (B === C && A !== B) {
+//   console.log(1);
+// } else {
+//   console.log("Xatolik: Faqat bitta son boshqacha bolishi kerak!");
+// }
 
 // 19-Misol!
-let b = 1245;
-console.log(parseInt(b / 60));
+// let A = +prompt("A:");
+// let B = +prompt("B:");
+// let C = +prompt("C:");
+// let D = +prompt("D:");
+
+// if (A === B && B === C && C !== D) {
+//   console.log(4);
+// } else if (A === B && B === D && D !== C) {
+//   console.log(3);
+// } else if (A === C && C === D && D !== B) {
+//   console.log(2);
+// } else if (B === C && C === D && D !== A) {
+//   console.log(1);
+// } else {
+//   console.log("Xato: uchta son teng bolishi kerak!");
+// }
 
 // 20-Misol!
-let vv = 43985;
-let bb = vv / 60;
-let nn = bb / 60;
-console.log(parseInt(nn));
+let A = +prompt("A:");
+let B = +prompt("B:");
+let C = +prompt("C:");
 
-// 21-Misol!
-let c = 754;
-console.log(parseInt(c / 60) + " daqiqa o'tdi.");
-console.log(c % 60 + " soniya o'tdi.");
+let dB = (A > B) ? A - B : B - A;
+let dC = (A > C) ? A - C : C - A;
 
-// 22-Misol!
-let MKL = 12345;
-let SDS = MKL / 60;
-let HMN = SDS / 60
-let WWD = (((SDS / 60) * 100) % 100) / 60 ;
-console.log(parseInt(HMN));
-console.log((WWD));
-
-// 23-Misol!
-let ASD = 76541;
-const DED = ASD / 60;
-const WDW = DED * 100 % 100
-const FDF = DED / 60;
-console.log("Hour: ", parseInt(FDF), "Minute: ", parseInt(DED), "Second: ", parseInt(WDW))
+if (dB < dC) {
+  console.log("Eng yaqin nuqta: B");
+  console.log("Masofa:", dB);
+} else if (dC < dB) {
+  console.log("Eng yaqin nuqta: C");
+  console.log("Masofa:", dC);
+} else {
+  console.log("B va C A ga teng masofada.");
+  console.log("Masofa:", dB);
+}
