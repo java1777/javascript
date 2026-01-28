e21c86365720888a25fec06666f793f1f3985a8b
Javohir <108736263+java1777@users.noreply.github.com>
2026-01-28 20:27:37 +0500

    xali tugamagan tekshirmi turin ustoz

--- src/main.js
+++ src/main.js
@@ -1,28 +1,5 @@
-const productInputElement = document.getElementById("product");
-const addBtnElement = document.getElementById("add-btn");
-const listElement = document.getElementById("list");
+const green = document.getElementById("green");
 
-window.addEventListener("load", () => {
-  const products = JSON.parse(localStorage.getItem("products")) || [];
-  products.forEach((product) => {
-    const newItem = document.createElement("li");
-    newItem.textContent = product;
-    listElement.appendChild(newItem);
-  });
-});
-
-addBtnElement.addEventListener("click", () => {
-  const value = productInputElement.value.trim();
-
-  if (value) {
-    const newItem = document.createElement("li");
-    newItem.textContent = value;
-    listElement.appendChild(newItem);
-
-    let products = JSON.parse(localStorage.getItem("products")) || [];
-    products.push(value);
-    localStorage.setItem("products", JSON.stringify(products));
-
-    productInputElement.value = "";
-  }
+green.addEventListener("click", () => {
+  document.body.style.backgroundColor = "green";
 });
